d4e419fc975feba9721acf078c27c23bcf00e1e7
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-05-20 12:30:05 +0300

    Update Date Audit

--- src/main/java/com/example/pollingApp/config/AuditingConfig.java
+++ src/main/java/com/example/pollingApp/config/AuditingConfig.java
@@ -1,4 +1,4 @@
-package com.example.pollingApp;
+package com.example.pollingApp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
